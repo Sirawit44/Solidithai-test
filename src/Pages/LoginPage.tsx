fd69a589fc58921db4ff1a271a4a3e008c1d263e
Sirawit44 <164641741+Sirawit44@users.noreply.github.com>
2024-09-25 03:54:11 +0700

    add topic to login page

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -4,7 +4,8 @@ import RegisterContainer from "../features/authentication/components/RegisterCon
 export default function LoginPage() {
   return (
     <div className="page-container">
-      <div className="bg-pink-300 p-4 rounded-lg max-w-sm mx-auto shadow-lg mt-60">
+      <div className="bg-pink-300 p-4 rounded-lg w-[70%] max-w-md shadow-lg absolute self-center">
+        <h5 style={{textAlign: "center"}}>LOGIN</h5>
         <LoginForm/>
         <RegisterContainer/>
       </div>
