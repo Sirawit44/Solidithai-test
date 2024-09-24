d29f090d4288ed8c6ee3439e606f8df6f023f4d1
Sirawit44 <164641741+Sirawit44@users.noreply.github.com>
2024-09-25 03:54:46 +0700

    add mock server

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+if (process.env.NODE_ENV === 'development') {
+  // This will start the service worker
+  import('./mocks/browser').then(({ worker }) => {
+      worker.start();
+  });
+}
+
+
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
+  <StrictMode>
     <App />
-  // </StrictMode>,
+  </StrictMode>,
 )
